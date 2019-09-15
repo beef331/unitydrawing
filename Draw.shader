d45fe818eaebf4537743fbee7474f53939c88f69
Beef331 <beefers331@gmail.com>
2019-09-15 01:52:39 -0600

    Added code/shader

---
+++ Draw.shader
@@ -0,0 +1,103 @@
+﻿Shader "Hidden/Draw"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            float MDistance(float2 pointA,float2 pointB){
+                return abs(pointA.x - pointB.x) + abs(pointA.y-pointB.y);
+            }
+
+
+            sampler2D _MainTex;
+            float4 _MainTex_TexelSize;
+            float4 _MousePos;
+            float4 _LastPos;
+            float4 _Color;
+            float _Decay;
+            fixed4 frag (v2f i) : SV_Target
+            {
+                for(int j=0; j < 100;j++){
+                    float2 mousePos = lerp(_LastPos.xy,_MousePos.xy,j/100.0);
+                    float2 delta = abs(mousePos.xy - i.uv);
+                    delta.x *= (_ScreenParams.x/_ScreenParams.y);
+                    if(length(delta) <= lerp(_LastPos.z,_MousePos.z,j/100.0) * _MainTex_TexelSize.x){
+                        fixed4 col = _Color;
+                        _Color.a = 1;
+                        return _Color;
+                    }
+                }
+                fixed4 pix = tex2D(_MainTex,i.uv);
+                return saturate(pix + (unity_DeltaTime.x * _Decay));
+            }
+            ENDCG
+        }
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+            
+            fixed4 frag (v2f i) : SV_Target
+            {
+                return 1;
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Drawer.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawer : MonoBehaviour
+{
+
+    [SerializeField]
+    private float brushRadius = 5;
+    [SerializeField]
+    private float decaySpeed = .5f;
+    private Color[] cols = new Color[8];
+    private int currentCol = 0;
+    [SerializeField]
+    private RenderTexture rt;
+    private Material material;
+    private Vector4 lastPos;
+    [SerializeField]
+    private float colSwitchDelay = .1f;
+    private float lastSwitch = 0;
+    [SerializeField]
+    private float maxSize = 200, minSize = 10;
+
+
+    void Start()
+    {
+        rt = new RenderTexture(Screen.width, Screen.height, 0);
+        rt.Create();
+        material = new Material(Shader.Find("Hidden/Draw"));
+        material.SetFloat("_Decay", decaySpeed);
+        Clear();
+    }
+
+    void InitColors()
+    {
+        for (int i = 0; i < cols.Length; i++)
+        {
+            cols[i] = new Color(Random.value + .1f, Random.value + .1f, Random.value + .1f, 1);
+        }
+        material.SetColor("_Color", cols[0]);
+    }
+
+    void Clear()
+    {
+        RenderTexture buffer = RenderTexture.GetTemporary(rt.descriptor);
+        Graphics.Blit(rt, buffer, material, 1);
+        Graphics.Blit(buffer, rt);
+        buffer.Release();
+        InitColors();
+    }
+    void DrawOnTex()
+    {
+        RenderTexture buffer = RenderTexture.GetTemporary(rt.descriptor);
+        Graphics.Blit(rt, buffer, material, 0);
+        Graphics.Blit(buffer, rt);
+        buffer.Release();
+    }
+
+    void Update()
+    {
+        Vector4 mousePos;
+        mousePos = new Vector4(Input.mousePosition.x / Screen.width, Input.mousePosition.y / Screen.height, brushRadius, 0);
+        material.SetVector("_MousePos", mousePos);
+        if (Time.time - lastSwitch >= colSwitchDelay)
+        {
+            currentCol = (currentCol + cols.Length + 1) % cols.Length;
+            lastSwitch = Time.time;
+        }
+        material.SetColor("_Color", Color.Lerp(cols[currentCol], cols[(currentCol + 1 + cols.Length) % cols.Length], (Time.time - lastSwitch) / colSwitchDelay));
+        //brushRadius = Mathf.Clamp(Mathf.Abs(Mathf.Sin(Time.time)) * maxSize, minSize, maxSize);
+        if(Input.GetKey(KeyCode.O)){
+                    brushRadius = Mathf.Clamp(brushRadius - Time.deltaTime * 20f, minSize, maxSize);
+        }
+        if(Input.GetKey(KeyCode.P)){
+                    brushRadius = Mathf.Clamp(brushRadius + Time.deltaTime * 20f, minSize, maxSize);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            DrawOnTex();
+        }
+        lastPos = mousePos;
+        material.SetVector("_LastPos", lastPos);
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            Clear();
+        }
+    }
+    void OnGUI()
+    {
+        Graphics.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), rt);
+    }
+}
